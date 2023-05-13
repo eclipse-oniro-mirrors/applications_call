8304565b9f2182124018b15f8c8f4a5463650d5e
18518779689 <litingting84@huawei.com>
2023-05-13 18:09:45 +0800

    update code callui:
    Signed-off-by: 18518779689 <litingting84@huawei.com>

--- entry/src/main/ets/MainAbility/MainAbility.ts
+++ entry/src/main/ets/MainAbility/MainAbility.ts
@@ -1,16 +1,14 @@
 import Ability from '@ohos.app.ability.UIAbility'
 import LogUtils from '../common/utils/LogUtils'
 import CallManager from '../model/CallManager'
-import GlobalThisHelper from '../common/utils/GlobalThisHelper'
-import Constants from '../common/utils/Constants'
 
 const TAG = "MainAbility";
 
 export default class MainAbility extends Ability {
   onCreate(want, launchParam) {
     LogUtils.i(TAG, "onCreate")
-    GlobalThisHelper.set<any>(Constants.GLOBALTHIS_ABILITY_WANT, want);
-    GlobalThisHelper.set<any>(Constants.GLOBALTHIS_CONTEXT, this.context);
+    globalThis.abilityWant = want;
+    globalThis.calluiAbilityContext = this.context;
   }
 
   onWindowStageCreate(windowStage) {
